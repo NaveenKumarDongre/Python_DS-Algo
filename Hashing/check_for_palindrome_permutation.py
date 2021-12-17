e53357d7334e1124050631d21720b2664e189e04
Naveen Kumar Dongre <naveenkumardongre1@gmail.com>
2021-12-17 13:52:30 +0530

    check_for_palindrome_permutation.py file added

---
+++ Hashing/check_for_palindrome_permutation.py
@@ -0,0 +1,42 @@
+from typing import Counter
+
+
+def is_palindrome_per(s):
+    # using a inbuilt Cointer method
+    cnt = Counter(s)
+    # print(cnt)
+    odd = 0
+    for freq in cnt.values():
+        # print(freq)
+        if freq % 2 != 0:
+            odd = odd + 1
+            if odd > 1:
+                return False
+
+    return True
+
+
+def is_palindrome_per_dict(s):
+    # making a dictanary count the frequency of the elements
+    cnt_freq = dict()
+    for i in s:
+        if i in cnt_freq:
+            cnt_freq[i] += 1
+        else:
+            cnt_freq[i] = 1
+    # print(cnt_freq)
+    odd = 0
+
+    for freq in cnt_freq.values():
+        # print(freq)
+        if freq % 2 != 0:
+            odd = odd + 1
+            # print(odd)
+            if odd > 1:
+                return False
+    return True
+
+
+s = "Nav"
+print(is_palindrome_per(s))
+print(is_palindrome_per_dict(s))
